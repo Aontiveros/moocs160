ade00ba1f89fbbde2a63f5528dad01e568584bae
Other <other@Edgar-M-MBP.local>
2015-11-04 08:27:39 -0800

    Added functionality to grab additional course information

--- Open2StudyScraper/src/main/java/Course.java
+++ Open2StudyScraper/src/main/java/Course.java
@@ -4,7 +4,13 @@ public class Course {
 	private String title;
 	private String description;
 	private String detailPage;
-	private String instructor;
+	private String instructorName;
+	private String startDate;
+	private String endDate;
+	private String whatIsItAbout;
+	private String whatWillILearn;
+	private String instructorBio;
+	
 	
 	public Course(String title, String description, String detailPage){
 		this.title = title;
@@ -12,8 +18,10 @@ public class Course {
 		this.detailPage = detailPage;
 	}
 	
+
+	
 	public void setInstructor(String instructor){
-		this.instructor = instructor;
+		this.instructorName = instructor;
 	}
 	
 	public String getDetailPageURL(){
@@ -23,7 +31,67 @@ public class Course {
 	public String toString(){
 		return "Title: " + title 
 				+ "\nDescription: " + description
-				+ "\nInstructor: " + instructor;
+				+ "\nInstructor: " + instructorName;
 	}
-	
+
+
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+
+
+	public String getWhatIsItAbout() {
+		return whatIsItAbout;
+	}
+
+
+
+	public void setWhatIsItAbout(String whatIsItAbout) {
+		this.whatIsItAbout = whatIsItAbout;
+	}
+
+
+
+	public String getWhatWillILearn() {
+		return whatWillILearn;
+	}
+
+
+
+	public void setWhatWillILearn(String whatWillILearn) {
+		this.whatWillILearn = whatWillILearn;
+	}
+
+
+
+	public String getInstructorBio() {
+		return instructorBio;
+	}
+
+
+
+	public void setInstructorBio(String instructorBio) {
+		this.instructorBio = instructorBio;
+	}
 }
+	
--- Open2StudyScraper/src/main/java/Parser.java
+++ Open2StudyScraper/src/main/java/Parser.java
@@ -6,12 +6,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class Parser {
+public class Main {
 
 	public static volatile boolean threadError = false;
 	
 	public static final String WEBSITE = "https://www.open2study.com/courses";
-	public static void main(String[] args) {
+	public static void Parser(String[] args) {
 		try{
 			System.out.println("Loading page...");
 			
@@ -21,26 +21,46 @@ public class Parser {
 			System.out.println("Parsing courses...");
 			ArrayList<Course> parsedCourseInfo = new ArrayList<Course>();
 			ArrayList<Thread> threads = new ArrayList<>();
+			
 			for (Element e : courses){
 				String title = e.getElementsByClass("adblock_course_title").first().ownText();
 				String description = e.getElementsByClass("adblock_course_body").first().ownText();
 				
 				
-				String detailPage = e.baseUri() + "/" + title.replace(' ', '-');
+				String detailPage = "https://www.open2study.com" + e.getElementsByTag("a").attr("href"); 
+						//e.baseUri() + "/" + title.replace(' ', '-');
 				
 				Course c = new Course(title, description, detailPage);
 				
-				Thread t = new Thread(new CourseDetailThread(c));
-				t.start();
-				threads.add(t);
-				
 				parsedCourseInfo.add(c);
-			}
-			
-			for (Thread t : threads){
-				t.join();
-				if (threadError)
-					throw new Exception("Error loading one of the course detail pages!");
+				
+				try {
+					doc = Jsoup.connect(c.getDetailPageURL()).followRedirects(true).get();
+					System.out.println("detail page = " + c.getDetailPageURL());
+					
+					Element nameElement = doc.getElementById("subject-teacher-tagline").firstElementSibling().getElementsByTag("h5").first();
+					
+					String instructorName = nameElement.text().substring(3);
+					String startDate = doc.getElementsByClass("offering_dates_date").first().text();
+					String endDate = doc.getElementsByClass("offering_dates_date").get(1).text();
+					String whatIsItAbout = doc.getElementsByClass("full-body").get(0).text();
+					String whatWillILearn = doc.getElementsByClass("whatyouwilllearncontainer").get(0).text();
+					String instructorBio = doc.getElementById("full-body429").text();
+					
+					c.setInstructor(instructorName);
+					c.setStartDate(startDate);
+					c.setEndDate(endDate);
+					c.setWhatIsItAbout(whatIsItAbout);
+					c.setWhatIsItAbout(whatWillILearn);
+					c.setInstructorBio(instructorBio);
+					
+					
+				} catch (Exception ex) {
+					System.out.println("Error" + c.getDetailPageURL());
+					System.out.println(ex.toString());
+					ex.printStackTrace();
+				}
+				
 			}
 			
 			for (Course c : parsedCourseInfo){
@@ -50,37 +70,8 @@ public class Parser {
 		catch(IOException e){
 			System.err.println("Problem connecting to " + WEBSITE);
 			e.printStackTrace();
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		} catch (Exception e1) {
-			e1.printStackTrace();
 		}
 	}
 	
-	public static class CourseDetailThread implements Runnable{
-
-		private Course c;
-		
-		public CourseDetailThread(Course c){
-			this.c = c;
-		}
-		@Override
-		public void run() {
-			try {
-				Document doc = Jsoup.connect(c.getDetailPageURL()).followRedirects(true).get();
-				
-				
-				String name = doc.select("h3").first().text();
-				
-				c.setInstructor(name);
-				
-			} catch (Exception e) {
-				System.out.println(c.getDetailPageURL());
-				e.printStackTrace();
-			}
-			
-		}
-		
-	}
 
 }
